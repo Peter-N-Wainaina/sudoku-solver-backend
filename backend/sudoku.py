c0557942187feeede5feea7b3712fda714951ea7
Peter N Wainaina <ngangapeter1115@gmail.com>
2024-05-09 02:03:47 -0400

    Implemented sudoku class functions
    - Refactored into frontend and backend
    - Fixed some sudoku functions and added tests

--- backend/sudoku.py
+++ backend/sudoku.py
@@ -10,16 +10,14 @@ class Sudoku(object):
                   for a 9 by 9 board.
         Precondition: k and m must fully divide n 
         """
-        self.print_board(board)
-
+        assert len(board) % box_dims[0] == 0 and len(board)% box_dims[1] == 0
         if self._is_valid_board(board, box_dims): #TODO:Maybe change to a try block 
             self.board = board
             self.size = len(board)
             self.box_height, self.box_width = box_dims
-            self.row_bags = [set()] * self.size
-            self.col_bags = [set()] * self.size
-            self.box_bags = [set()] * ((self.size / self.box_height) *
-                                        (self.size / self.box_width)) #TODO: How to index into these boxes
+            self.row_bags = [set() for _ in range(self.size)]
+            self.col_bags = [set() for _ in range( self.size)] 
+            self.box_bags = [set() for _ in range ( int((self.size / self.box_height) * (self.size / self.box_width)))]  
             self._fill_bags()
         else:
             raise self.InvalidBoard
@@ -30,18 +28,19 @@ class Sudoku(object):
     def _get_box(self, cell):
         r, c = cell
         boxes_across = self.size / self.box_width
-        return ((floor(r / self.size)) * boxes_across) + floor(c / self.size)
+        boxes_down = self.size / self.box_height
+        return int((floor(r / boxes_down)) * boxes_across) + floor(c / boxes_across)
 
     def _fill_bags(self):
-        #fill rows:
         for row in range(self.size):
             for col in range(self.size):
                 cell = self.board[row][col]
                 if cell is not None: 
                     self.row_bags[row].add(cell)
                     self.col_bags[col].add(cell)
-                    self.box_bags[self._get_box((row, col))].add(cell)
-
+                    box_idx = self._get_box((row, col))
+                    self.box_bags[box_idx].add(cell)
+                    
     def possible_moves(self):
         """
         Returns a list of triples (i, j, k). (i,j) is the position in board where
@@ -59,6 +58,34 @@ class Sudoku(object):
                 for move in cell_moves:
                     pos_moves.append((row, col, move))
         return pos_moves
+    
+    def make_move(self, move):
+        i, j, k = move
+        self.board[i][j] = k
+        b_idx = self._get_box((i,j))
+        self.row_bags[i].add(k)
+        self.col_bags[j].add(k)
+        self.box_bags[b_idx].add(k)
+
+    def undo_move(self, move):
+        i, j, k = move 
+        self.board[i][j] = None
+        b_idx = self._get_box((i,j))
+        self.row_bags[i].remove(k)
+        self.col_bags[j].remove(k)
+        self.box_bags[b_idx].remove(k)
+
+    def is_solved(self):
+        """
+        Returns True if board is solved 
+        """
+        raise NotImplemented
+    
+    def solve(self):
+        """
+        Solves self.board
+        """
+        raise NotImplemented
 
     def _is_valid_board(self, board, box_dims):
         """
@@ -67,18 +94,16 @@ class Sudoku(object):
         n = len(board)
         box_row, box_col = box_dims
 
-        def check_invalid_cell(cell, bag):
-           return (type(cell) != int and cell is not None) or (cell not in range (1, n + 1)) or  (cell in bag) #TODO:Fix this!!! 
-
+        def invalid_cell(cell, bag):
+           return not ( (type(cell) == int and (cell in range (1, n + 1)) and cell not in bag )  or cell is None)
+            
         def check_rows(board):
             for row in board:
                 r_bag = set()
                 for cell in row:
-                    if check_invalid_cell(cell,r_bag):
-                        print("This is the issue: ",cell)
+                    if invalid_cell(cell,r_bag):
                         return False
                     r_bag.add(cell)
-            print("Row: This is True")
             return True
                     
         def check_boxes(board):
@@ -89,24 +114,23 @@ class Sudoku(object):
                     for row in range(start_row, start_row + box_row):
                         for col in range(start_col, start_col + box_col):
                             cell = board[row][col]
-                            if check_invalid_cell(cell,box_bag):
-                                print("Box: This is False")
+                            if invalid_cell(cell,box_bag):
                                 return False
                             box_bag.add(cell)
-            print("Box: This is true")
             return True
-        return check_rows(board) and check_rows(np.array(board).T) and check_boxes(board)
+        b_transposed = [[row[i] for row in board] for i in range(len(board[0]))]
+        return check_rows(board) and check_rows(b_transposed) and check_boxes(board)
 
-    def print_board(self, board):
+    def print_board(self):
         """
         Prints a formatted  n by n board to terminal
         """
-        n = len(board)
+        n = self.size
         b_str = ""
         for i in range(n):
             row = "|"
             for j in range(n):
-                cell = str(board[i][j])
+                cell = str(self.board[i][j])
                 row +=  cell + " |"
             b_str += (row  + "\n")
             b_str += ("_"*len(row) + "\n")
---
+++ backend/sudoku_test.py
@@ -0,0 +1,53 @@
+import sudoku
+
+
+def read_board(text_file):
+    try: 
+        with open(text_file) as file:
+            contents = file.readlines()
+            return contents
+    except Exception as e:
+        print(f"File Open Failed: {e}")
+
+def create_board_from_file(file_name,board_size):
+    contents = read_board(file_name)
+    board = [[None for _ in range(board_size)] for _ in range(board_size)]
+    for line in contents:
+        line = line.replace(",","").strip()
+        i,j,k = int(line[0]),int(line[1]),int(line[2])
+        board[i][j] = k
+    return board
+
+def compare_lists(list1, list2):
+    """
+    Returns True if the lists have the same elements. 
+    Order doesn't matter
+    """
+    return set(list1)== set(list2)
+
+valid_1 = create_board_from_file("data/valid_board_1.txt", 9)
+valid_1 = sudoku.Sudoku(valid_1, (3,3))
+assert valid_1.possible_moves() == []
+assert valid_1._get_box((0,3)) == 1
+assert valid_1._get_box((8,7)) == 8
+assert valid_1._get_box((5,5)) == 4
+
+
+valid_2 = create_board_from_file("data/valid_board_2.txt", 9)
+valid_2 = sudoku.Sudoku(valid_2, (3,3))
+moves = valid_2.possible_moves()
+assert  compare_lists(moves, [ (8,7,7), (8,8,9), (5,5,4)])
+row = [1,2,3,4,5,6,8]
+assert compare_lists(row, valid_2.row_bags[8])
+
+valid_2.make_move((8,8,9))
+moves = valid_2.possible_moves()
+assert  compare_lists(moves, [ (8,7,7), (5,5,4)])
+row = [1,2,3,4,5,6,8,9]
+box = [1,2,3,4,5,6,8,9]
+assert compare_lists(row, valid_2.row_bags[8])
+assert compare_lists(box, valid_2.box_bags[8])
+
+valid_2.undo_move((0,0,5))
+moves = valid_2.possible_moves()
+assert  compare_lists(moves, [ (8,7,7), (0,0,5), (5,5,4)])
